a1ed24090e40cd476fd29e03e6dfaf7775b40da8
Ryan Winograd <ryan@thewinograds.com>
2016-06-01 01:23:22 -0500

    Use long-lived connection to rot13 process

--- background.js
+++ background.js
@@ -5,14 +5,18 @@ function checkForValidUrl(tabId, changeInfo, tab) {
 };
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
 
+var rot13Port = chrome.runtime.connectNative('com.valencepm.rot13');
+rot13Port.onMessage.addListener(function(msg, sender, sendResponse) {
+  console.log("Received: " + JSON.stringify(msg));
+  if (this.contentScriptPort) {
+    this.contentScriptPort.postMessage(msg.zrffntr);
+  }
+});
+
 chrome.runtime.onConnect.addListener(function(port) {
+  var contentScriptPort = null;
   port.onMessage.addListener(function(msg) {
-    chrome.runtime.sendNativeMessage(
-      "com.valencepm.rot13",
-      { message: msg },
-      function(response) {
-        port.postMessage(response.zrffntr);
-      }
-    );
+    this.contentScriptPort = port;
+    rot13Port.postMessage({ message: msg });
   });
 });
